8f1605349bd8864638396821fcc8a3c6d964da8a
Matthew Ingle <mingle@riotgames.com>
2022-01-13 15:51:26 +0000

    Added filter as a field on infrastructure conditon

--- pkg/alerts/infrastructure_conditions.go
+++ pkg/alerts/infrastructure_conditions.go
@@ -9,25 +9,25 @@ import (
 
 // InfrastructureCondition represents a New Relic Infrastructure alert condition.
 type InfrastructureCondition struct {
-	Comparison          string                            `json:"comparison,omitempty"`
-	CreatedAt           *serialization.EpochTime          `json:"created_at_epoch_millis,omitempty"`
-	Critical            *InfrastructureConditionThreshold `json:"critical_threshold,omitempty"`
-	Enabled             bool                              `json:"enabled"`
-	Event               string                            `json:"event_type,omitempty"`
-	Filter              map[string][]map[string]string    `json:"filter,omitempty"`
-	ID                  int                               `json:"id,omitempty"`
-	IntegrationProvider string                            `json:"integration_provider,omitempty"`
-	Name                string                            `json:"name,omitempty"`
-	PolicyID            int                               `json:"policy_id,omitempty"`
-	ProcessWhere        string                            `json:"process_where_clause,omitempty"`
-	RunbookURL          string                            `json:"runbook_url,omitempty"`
-	Select              string                            `json:"select_value,omitempty"`
-	Type                string                            `json:"type,omitempty"`
-	UpdatedAt           *serialization.EpochTime          `json:"updated_at_epoch_millis,omitempty"`
-	ViolationCloseTimer *int                              `json:"violation_close_timer,omitempty"`
-	Warning             *InfrastructureConditionThreshold `json:"warning_threshold,omitempty"`
-	Where               string                            `json:"where_clause,omitempty"`
-	Description         string                            `json:"description"`
+	Comparison          string                                    `json:"comparison,omitempty"`
+	CreatedAt           *serialization.EpochTime                  `json:"created_at_epoch_millis,omitempty"`
+	Critical            *InfrastructureConditionThreshold         `json:"critical_threshold,omitempty"`
+	Enabled             bool                                      `json:"enabled"`
+	Event               string                                    `json:"event_type,omitempty"`
+	Filter              map[string][]map[string]map[string]string `json:"filter,omitempty"`
+	ID                  int                                       `json:"id,omitempty"`
+	IntegrationProvider string                                    `json:"integration_provider,omitempty"`
+	Name                string                                    `json:"name,omitempty"`
+	PolicyID            int                                       `json:"policy_id,omitempty"`
+	ProcessWhere        string                                    `json:"process_where_clause,omitempty"`
+	RunbookURL          string                                    `json:"runbook_url,omitempty"`
+	Select              string                                    `json:"select_value,omitempty"`
+	Type                string                                    `json:"type,omitempty"`
+	UpdatedAt           *serialization.EpochTime                  `json:"updated_at_epoch_millis,omitempty"`
+	ViolationCloseTimer *int                                      `json:"violation_close_timer,omitempty"`
+	Warning             *InfrastructureConditionThreshold         `json:"warning_threshold,omitempty"`
+	Where               string                                    `json:"where_clause,omitempty"`
+	Description         string                                    `json:"description"`
 }
 
 // InfrastructureConditionThreshold represents an New Relic Infrastructure alert condition threshold.
